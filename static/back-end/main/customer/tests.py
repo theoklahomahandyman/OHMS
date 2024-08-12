e674dd0efd61f3d3e4974f0afb2896b76c504699
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-08-12 12:25:06 -0500

    adds serializer for customer model with test coverage

--- static/back-end/main/customer/tests.py
+++ static/back-end/main/customer/tests.py
@@ -1,4 +1,5 @@
 from rest_framework.test import APITestCase, APIClient
+from customer.serializers import CustomerSerializer
 from customer.models import Customer
 from rest_framework import status
 from django.test import TestCase
@@ -14,3 +15,98 @@ class TestCustomerModel(TestCase):
     ## Test string method for user model
     def test_user_string(self):
         self.assertEqual(str(self.customer), f'{self.customer.first_name} {self.customer.last_name}')
+
+# Tests for customer serializer
+class TestCustomerSerializer(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.long_string = 'a' * 256
+        cls.customer = Customer.objects.create(first_name='first', last_name='last', email='firstlast@example.com', phone='1 (234) 567-8901')
+        cls.empty_data = {
+            'first_name': '',
+            'last_name': '',
+            'email': '',
+            'phone': '',
+        }
+        cls.short_data = {
+            'first_name': 'J',
+            'last_name': 'D',
+            'email': 'j@e.com',
+            'phone': '183043',
+        }
+        cls.long_data = {
+            'first_name': cls.long_string,
+            'last_name': cls.long_string,
+            'email': cls.long_string + '@example.com',
+            'phone': cls.long_string,
+        }
+        cls.create_data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'johndoe@example.com',
+            'phone': '1 (586) 375-3896',
+        }
+        cls.update_data = {
+            'first_name': 'Bill',
+            'last_name': 'Johnson',
+            'email': 'billjohnson@example.com',
+            'phone': '45 (192) 937-9329',
+        }
+
+    # Test validate with empty data
+    def test_validate_empty_data(self):
+        serializer = CustomerSerializer(data=self.empty_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('first_name', serializer.errors)
+        self.assertIn('last_name', serializer.errors)
+        self.assertIn('email', serializer.errors)
+        self.assertIn('phone', serializer.errors)
+
+    # Test validate with data too long
+    def test_validate_long_data(self):
+        serializer = CustomerSerializer(data=self.long_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('first_name', serializer.errors)
+        self.assertIn('last_name', serializer.errors)
+        self.assertIn('email', serializer.errors)
+        self.assertIn('phone', serializer.errors)
+
+    # Test validate with data too short
+    def test_validate_short_data(self):
+        serializer = CustomerSerializer(data=self.short_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('first_name', serializer.errors)
+        self.assertIn('last_name', serializer.errors)
+        self.assertIn('email', serializer.errors)
+        self.assertIn('phone', serializer.errors)
+
+    # Test validate with incorrectly formatted phone number
+    def test_validate_incorrectly_formatted_phone(self):
+        self.create_data['phone'] = '+51-234-567-8901'
+        serializer = CustomerSerializer(data=self.create_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('phone', serializer.errors)
+
+    # Test create success
+    def test_create_success(self):
+        serializer = CustomerSerializer(data=self.create_data)
+        self.assertTrue(serializer.is_valid())
+        customer: Customer = serializer.save()
+        customer.refresh_from_db()
+        self.assertEqual(Customer.objects.count(), 2)
+        self.assertEqual(customer.first_name, self.create_data['first_name'])
+        self.assertEqual(customer.last_name, self.create_data['last_name'])
+        self.assertEqual(customer.email, self.create_data['email'])
+        self.assertEqual(customer.phone, self.create_data['phone'])
+
+    # Test update success
+    def test_update_success(self):
+        serializer = CustomerSerializer(instance = self.customer, data=self.update_data)
+        self.assertTrue(serializer.is_valid())
+        customer: Customer = serializer.save()
+        customer.refresh_from_db()
+        self.assertEqual(customer.first_name, self.update_data['first_name'])
+        self.assertEqual(customer.last_name, self.update_data['last_name'])
+        self.assertEqual(customer.email, self.update_data['email'])
+        self.assertEqual(customer.phone, self.update_data['phone'])
