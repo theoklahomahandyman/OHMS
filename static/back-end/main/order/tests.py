31174c8114ef82b5444cb719a10a7117da81c8ef
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-08-20 23:06:39 -0500

    adds serializers for order models and sets up test classes for order models and individual serializers

--- static/back-end/main/order/tests.py
+++ static/back-end/main/order/tests.py
@@ -1,3 +1,39 @@
+from order.models import Order, OrderCost, OrderPicture, OrderMaterial, OrderPayment
 from django.test import TestCase
 
 # Create your tests here.
+class TestOrderModels(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+class TestOrderSerializer(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+class TestOrderCostSerializer(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+class TestOrderPictureSerializer(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+class TestOrderMaterialSerializer(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+class TestOrderPaymentSerializer(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        pass
