dbe3899f174935f1bc940381d73daf715ee8b8c8
unknown <dylanmercer12@icloud.com>
2024-12-20 15:23:15 -0600

    updates docker ignore file, and settings.py to use env variables for allowed hosts, debug, and django secret key

--- back-end/main/settings.py
+++ back-end/main/settings.py
@@ -14,12 +14,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w5_t$fb#)xzvwud1f$)^64i&i2ml_-+!earyo1y7k*abuc8(94'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
+if ALLOWED_HOSTS_ENV:
+    ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
 
 
 # Application definition
