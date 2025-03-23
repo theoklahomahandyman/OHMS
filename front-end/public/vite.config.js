a0cc01d8b7811fd88b841c1be0fd8e7d3de8ad11
unknown <dylanmercer12@icloud.com>
2025-03-22 23:40:11 -0500

    makes seo changes

--- front-end/public/vite.config.js
+++ front-end/public/vite.config.js
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-bootstrap')) return 'react-bootstrap';
+            if (id.includes('bootstrap')) return 'bootstrap';
+            if (id.includes('react-icons')) return 'react-icons';
+            return 'vendor';
+          }
+        },
+        entryFileNames: `[name].[hash].js`,
+        chunkFileNames: `[name].[hash].js`,
+        assetFileNames: `[name].[hash].[ext]`
+      }
+    },
+    brotliSize: true,
+    chunkSizeWarningLimit: 1000
+  }
+});
